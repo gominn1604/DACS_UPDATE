f20d2acc256f1148fb19cf84f734e1b2f457f58b
Duy Danh <duydanh16042019@gmail.com>
2021-12-22 22:43:53 +0700

    ad

---
+++ MilkteaShopManager/DataAccess/Staff.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess
+{
+    public class Staff
+    {
+        public string MaNV { get; set; }
+        public string HoTen { get; set; }
+        public string DiaChi { get; set; }
+        public string GioiTinh { get; set; }
+        public DateTime NgaySinh { get; set; }
+        public string SDT { get; set; }
+        public string TenTaiKhoan { get; set; }
+        public bool TrangThai { get; set; }
+        public int MaCV { get; set; }
+    }
+}
---
+++ MilkteaShopManager/MilkteaShopManager/Form/ThanhToan.cs
@@ -0,0 +1,100 @@
+﻿using BusinessLogic;
+using DataAccess;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MilkteaShopManager
+{
+    public partial class FormThanhToan : Form
+    {
+        Table _table = new Table();
+        int _tongTien;
+        public FormThanhToan()
+        {
+            InitializeComponent();
+        }
+        public void LoadForm(Table table, int tongTien)
+        {
+            int tongThanhToan;
+            txtCongTien.Text = tongTien.ToString("###,### đ");
+            _tongTien = tongTien;
+            _table = table;
+            lblSoBan.Text = table.Name;
+
+            tongThanhToan = tongTien - Convert.ToInt32(mtxtGiamGia.Text) + Convert.ToInt32(txtThue.Text);
+            txtTongThanhToan.Text = tongThanhToan.ToString("###,### đ");
+        }
+
+        private void nudGiamGia_ValueChanged(object sender, EventArgs e)
+        {
+            int giamGia;
+            giamGia = _tongTien / 100 * Convert.ToInt32(nudGiamGia.Value);
+            mtxtGiamGia.Text = giamGia.ToString();
+
+            int tongThanhToan = _tongTien - Convert.ToInt32(mtxtGiamGia.Text) + Convert.ToInt32(txtThue.Text) + Convert.ToInt32(txtChiPhiKhac.Text);
+            txtTongThanhToan.Text = tongThanhToan.ToString("###,### đ");
+        }
+
+        private void cbbThue_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int thue;
+            thue = _tongTien / 100 * Convert.ToInt32(cbbThue.SelectedItem.ToString());
+            if (cbbThue.SelectedIndex == 0)
+            {
+                txtThue.Text = "0";
+            }    
+            else
+            {
+                txtThue.Text = thue.ToString();
+            }
+            int tongThanhToan = _tongTien - Convert.ToInt32(mtxtGiamGia.Text) + Convert.ToInt32(txtThue.Text) + Convert.ToInt32(txtChiPhiKhac.Text);
+            txtTongThanhToan.Text = tongThanhToan.ToString("###,### đ");
+        }
+
+        private void txtChiPhiKhac_TextChanged(object sender, EventArgs e)
+        { 
+            if (txtChiPhiKhac.Text == "" || IsNumber(txtChiPhiKhac.Text) == false)
+            {
+                txtChiPhiKhac.Text = "0";
+            }
+                int tongThanhToan = _tongTien - Convert.ToInt32(mtxtGiamGia.Text) + Convert.ToInt32(txtThue.Text) + Convert.ToInt32(txtChiPhiKhac.Text);
+                txtTongThanhToan.Text = tongThanhToan.ToString("###,### đ");
+        }
+
+        public bool IsNumber(string pValue)
+        {
+            foreach (Char c in pValue)
+            {
+                if (!Char.IsDigit(c))
+                    return false;
+            }
+            return true;
+        }
+
+        private void btnThanhToan_Click(object sender, EventArgs e)
+        {
+            BillBL billBL = new BillBL();
+            BillDA billDA = new BillDA();
+
+            int billId = billBL.GetUncheckBillIdByTableId(_table.ID);
+            int discount = Convert.ToInt32(mtxtGiamGia.Text);
+            int tax = Convert.ToInt32(txtThue.Text);
+
+            if (billId != -1)
+            {
+                if (MessageBox.Show("Bạn có muốn thanh toán hóa đơn cho " + _table.Name.ToLower(), "Thông báo", MessageBoxButtons.OKCancel) == DialogResult.OK)
+                {
+                    billDA.CheckOut(billId, discount, tax, _tongTien);
+                    this.Close();
+                }
+            }
+        }
+    }
+}
