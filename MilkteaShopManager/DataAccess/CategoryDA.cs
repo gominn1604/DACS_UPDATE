ffff31c641a5a9f6d5bfcb428e58db8a4c42d2e9
Bẩu <62896400+KinKute@users.noreply.github.com>
2021-12-22 21:44:55 +0700

    Bau_Update

---
+++ MilkteaShopManager/DataAccess/CategoryDA.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess
+{
+    public class CategoryDA
+    {
+        public List<Category> GetListCategory()
+        {
+            SqlConnection conn = new SqlConnection(Ultilities.ConnectionString);
+            conn.Open();
+
+            SqlCommand cmd = conn.CreateCommand();
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.CommandText = Ultilities.GetListCategory;
+
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            DataTable data = new DataTable();
+
+            da.Fill(data);
+            conn.Close();
+
+            List<Category> listCategory = new List<Category>();
+            foreach (DataRow item in data.Rows)
+            {
+                Category category = new Category(item);
+                listCategory.Add(category);
+            }
+            return listCategory;
+        }
+    }
+}
