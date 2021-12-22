d4e6e5aeaf383773e52e5fd2349686696cc8c255
Bẩu <62896400+KinKute@users.noreply.github.com>
2021-12-22 21:27:23 +0700

    Bau_Update

--- MilkteaShopManager/DataAccess/Table.cs
+++ MilkteaShopManager/DataAccess/Table.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -30,6 +31,27 @@ namespace DataAccess
             this.ID = (int)row["MaBan"];
             this.Name = row["TenBan"].ToString();
             this.Status = (int)row["TrangThaiBan"];
+        }
+        public int Insert_Update_Delete(Table table, int action)
+        {
+            SqlConnection sqlConn = new SqlConnection(Ultilities.ConnectionString);
+            sqlConn.Open();
+
+            SqlCommand cmd = sqlConn.CreateCommand();
+            cmd.CommandText = Ultilities.Table_InsertUpdateDelete;
+            cmd.CommandType = CommandType.StoredProcedure;
+
+            SqlParameter IDPara = new SqlParameter("@MaBan", SqlDbType.Int);
+            IDPara.Direction = ParameterDirection.InputOutput;
+
+            cmd.Parameters.Add(IDPara).Value = table.ID;
+            cmd.Parameters.Add("@TenBan", SqlDbType.NVarChar, 100).Value = table.Name;
+            cmd.Parameters.Add("@Action", SqlDbType.Int).Value = action;
+
+            int result = cmd.ExecuteNonQuery();
+            if (result > 0)
+                return (int)cmd.Parameters["@Maban"].Value;
+            return 0;
         }
     }
 }
